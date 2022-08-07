b38ed2f05a2a54d6bdcd942266a4c4fae86f242f
Vivek Kumar Sah <79993217+VivekKSah@users.noreply.github.com>
2022-08-07 16:02:00 +0530

    store data in database
    donorDetails.txt  is used to store data in database.

--- database.py
+++ database.py
@@ -1,25 +1,45 @@
-import psycopg2
+"""code to insert data in database"""
 
-#establishing the connection
+# pylint: disable=import-error
+
+import psycopg2
+from psycopg2.extensions import AsIs
+import sys
+import ast
+
+with open(sys.argv[1], 'r') as details:
+    detail_list=[]
+    for line in details:
+        detail_list.append(line.rstrip())
+
+# establishing the connection
 connection = psycopg2.connect(
-   database="test_database", user='test_user', password='test@123', host='127.0.0.1', port= '5432'
+    database="test_database",
+    user="test_user",
+    password="test@123",
+    host="127.0.0.1",
+    port="5432",
 )
 
-#Setting auto commit false
+# Setting auto commit false
 connection.autocommit = False
 
-#Creating a cursor object using the cursor() method
+# Creating a cursor object using the cursor() method
 cursor = connection.cursor()
 
 # Preparing SQL queries to INSERT a record into the database.
-cursor.execute('''INSERT INTO netbsd.paypal_donation_details(confirmation_no, contributor, amount, currency, quantity, email, donation_date)
-        VALUES ('05156965ED4410053', 'Josef Burger', 10.00, 'U.S. Dollars', 1, 'bolo@bolo.com', 'datetime.datetime(2022, 3, 25, 8, 9, 50, tzinfo=tzoffset(None, -25200))')''')
-cursor.execute('''INSERT INTO netbsd.stripe_donation_details(payment_id, contributor_email, amount)
-        VALUES ('ch_3KmQM12y9v89cZ3U1QEHNEbO', 'wadeamesbury@gmail.com', 10.00)''')
+for detail in detail_list:
+    detail=ast.literal_eval(detail)
+    columns = detail.keys()
+    values = [detail[column] for column in columns]
+    SQL = "INSERT INTO netbsd.donation_details (%s) VALUES %s"
+
+    # inserting records in the database
+    cursor.execute(SQL, (AsIs(",".join(columns)), tuple(values)))
 
 # Commit changes in the database
 connection.commit()
-print("Records inserted")
+print("Records inserted in the database")
 
-#Closing the connection
+# Closing the connection
 connection.close()
