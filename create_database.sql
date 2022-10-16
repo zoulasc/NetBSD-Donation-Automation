9e5dd15c17a7acfab6b88630c3ce41273179c8ff
Vivek Kumar Sah <79993217+VivekKSah@users.noreply.github.com>
2022-10-16 20:04:32 +0530

    Update create_database.sql

--- create_database.sql
+++ create_database.sql
@@ -25,7 +25,22 @@ CREATE TABLE netbsd.donation_details
   vendor character varying NOT NULL,
   datetime character varying,
   amount character varying NOT NULL,
-  CONSTRAINT payment_id PRIMARY KEY (confirmation_no)
+  CONSTRAINT feedback_id PRIMARY KEY (confirmation_no)
 );
 ALTER TABLE netbsd.donation_details
+  OWNER TO test_user;
+
+
+CREATE TABLE netbsd.feedbacks
+(
+  confirmation_no character varying NOT NULL,
+  answer1 boolean NOT NULL,
+  name character varying,
+  answer2 boolean NOT NULL,
+  email character varying,
+  answer3 boolean NOT NULL,
+  notificationi_email character varying,
+  CONSTRAINT payment_id PRIMARY KEY (confirmation_no)
+);
+ALTER TABLE netbsd.feedbacks
   OWNER TO test_user;
