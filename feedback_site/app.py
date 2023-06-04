d8351f1b7fe7cbd4e3750e37785a752505dbda3e
goksu <25721443+goeksu@users.noreply.github.com>
2023-06-04 17:57:21 +0300

    back to flask and api progs

--- feedback_site/app.py
+++ feedback_site/app.py
@@ -8,6 +8,7 @@ from configparser import ConfigParser
 
 app = Flask(__name__)
 
+
 def get_db_connection() -> psycopg2.extensions.connection:
     """establishing connection with database"""
     conn = psycopg2.connect(
@@ -19,16 +20,19 @@ def get_db_connection() -> psycopg2.extensions.connection:
     )
     return conn
 
+
 def sendmail(mail: str):
     config = ConfigParser()
     config.read("config.ini", encoding="utf-8")
     smtp_server = config["smtp"]["server"]
     port_no = config["smtp"]["port"]
     sender_email = config["email"]["sender"]
-    sender_password = os.environ['PASSWORD'] #SET ENV VARIABLE OR USE CONFIG.INI config["email"]["password"]
+    sender_password = os.environ[
+        "PASSWORD"
+    ]  # SET ENV VARIABLE OR USE CONFIG.INI config["email"]["password"]
     receiver_email = config["email"]["receiver"]
-    #sending all mails to the address defined in config.ini for test purposes
-    #receiver_email = data["email"] if not "Unknown"
+    # sending all mails to the address defined in config.ini for test purposes
+    # receiver_email = data["email"] if not "Unknown"
     ack_text = config["msg"]["text"]
     msg = ack_text
     context = ssl.create_default_context()
@@ -36,11 +40,10 @@ def sendmail(mail: str):
     server.login(sender_email, sender_password)
     server.sendmail(sender_email, receiver_email, msg)
     server.quit()
-    app.logger.info('\n--LOG: Mail Sent\n')
+    app.logger.info("\n--LOG: Mail Sent\n")
 
 
-
-SQL1 ="""
+SQL1 = """
 PREPARE check_donation_and_feedback (text, text) AS
 WITH match AS (
   SELECT d.email, d.confirmation_no
@@ -77,108 +80,106 @@ SELECT EXISTS(SELECT 1 FROM netbsd.feedbacks WHERE confirmation_no = $1);
 EXECUTE check_feedback('{fid}');
 """
 
-@app.route('/')
+
+@app.route("/")
 def index() -> str:
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/validate', methods=['POST'])
+@app.route("/validate", methods=["POST"])
 def validate() -> str:
-    fid=request.form['feed']
-    femail=request.form['email']
+    fid = request.form["feed"]
+    femail = request.form["email"]
     conn = get_db_connection()
     cur = conn.cursor()
-    
-    cur.execute(SQL1.format(fid=fid,mail=femail))
+
+    cur.execute(SQL1.format(fid=fid, mail=femail))
     databasequeries = cur.fetchall()
-    app.logger.info('\n--log: {0}\n'.format(databasequeries))
+    app.logger.info("\n--log: {0}\n".format(databasequeries))
 
     cur.close()
     conn.close()
 
-    #checks for if donation exists
+    # checks for if donation exists
     if not databasequeries[0][0]:
-        app.logger.info('\n--LOG: DONATION NOT FOUND\n')
-        return render_template('nodonation.html')
+        app.logger.info("\n--LOG: DONATION NOT FOUND\n")
+        return render_template("nodonation.html")
 
-    #checks for if feedback already exists
+    # checks for if feedback already exists
     if databasequeries[0][1]:
-        app.logger.info('\n--LOG: FEEDBACK ALREADY RECORDED\n')
-        return render_template('invalid.html',identifier=fid)
-    
-    return render_template('valid.html',fid=fid,femail=femail)
+        app.logger.info("\n--LOG: FEEDBACK ALREADY RECORDED\n")
+        return render_template("invalid.html", identifier=fid)
+
+    return render_template("valid.html", fid=fid, femail=femail)
 
 
-@app.route('/feedback')
+@app.route("/feedback")
 def feedbackbymail():
-    token = request.args.get('token')
+    token = request.args.get("token")
     try:
         uuid.UUID(token)
     except ValueError:
-        return render_template('nodonation.html')
-    
-    
+        return render_template("nodonation.html")
+
     conn = get_db_connection()
     cur = conn.cursor()
     cur.execute(SQL3.format(token=token))
     donation = cur.fetchall()
-    app.logger.info('\n--log: {0}\n'.format(donation))
+    app.logger.info("\n--log: {0}\n".format(donation))
     cur.close()
     conn.close()
     if not donation:
-        return render_template('nodonation.html')
-    
+        return render_template("nodonation.html")
 
     conn = get_db_connection()
     cur = conn.cursor()
     cur.execute(SQL4.format(fid=donation[0][0]))
     priordonation = cur.fetchall()
-    app.logger.info('\n--log: {0}\n'.format(priordonation))
+    app.logger.info("\n--log: {0}\n".format(priordonation))
     cur.close()
     conn.close()
     if priordonation[0][0]:
-        return render_template('invalid.html',identifier=donation[0][0])
-    return render_template('valid.html', fid=donation[0][0], femail=donation[0][1])
-    
+        return render_template("invalid.html", identifier=donation[0][0])
+    return render_template("valid.html", fid=donation[0][0], femail=donation[0][1])
 
-@app.route('/store/<string:fid>', methods=['POST'])
+
+@app.route("/store/<string:fid>", methods=["POST"])
 def store(fid: str) -> str:
-
-    answer1=request.form["answer1"]  
-    name=request.form["name"]
+    answer1 = request.form["answer1"]
+    name = request.form["name"]
     if not answer1 or not name:
         name = "Anonymous"
-        return render_template('thank_you.html')
-    
-    answer2=request.form["answer2"]
-    email=request.form["email"]
-    if not email or not answer2:
-        email="-"
+        return render_template("thank_you.html")
 
-    answer3=request.form["answer3"]
-    notification_email=request.form["notification_email"]
+    answer2 = request.form["answer2"]
+    email = request.form["email"]
+    if not email or not answer2:
+        email = "-"
+
+    answer3 = request.form["answer3"]
+    notification_email = request.form["notification_email"]
     if not notification_email or not answer2:
-        notification_email="-"
+        notification_email = "-"
 
     conn = get_db_connection()
     cur = conn.cursor()
-    cur.execute(SQL2.format(
+    cur.execute(
+        SQL2.format(
             fid=fid,
             ans1=answer1,
             name=name,
             ans2=answer2,
             mail1=email,
             ans3=answer3,
-            mail2=notification_email
-            )
+            mail2=notification_email,
         )
+    )
     cur.close()
     conn.commit()
     conn.close()
-    app.logger.info('\n--LOG: stored in db\n')
+    app.logger.info("\n--LOG: stored in db\n")
 
     if notification_email:
-        sendmail(notification_email) 
-   
-    return render_template('thank_you.html')
+        sendmail(notification_email)
 
+    return render_template("thank_you.html")
